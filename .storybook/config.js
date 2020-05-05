2553329461f8c7e2ba852bc635336d920b7d7af7
Harmeet Singh <hsingh@venacorp.com>
2020-05-05 15:14:46 -0400

    add PUBLIC_PATH for banner url

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,27 +1,27 @@
-import { configure, addDecorator, addParameters } from '@storybook/react';
-import { create } from '@storybook/theming';
+import { configure, addDecorator, addParameters } from "@storybook/react";
+import { create } from "@storybook/theming";
 
-import { withA11y } from '@storybook/addon-a11y';
+import { withA11y } from "@storybook/addon-a11y";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /\.stories\.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach((filename) => req(filename));
 }
 
 // Theme
 const theme = create({
   base: "light",
   brandTitle: "Kardia by Vena",
-  brandImage: "/kardia-banner.png" 
+  brandImage: process.env.PUBLIC_URL + "/kardia-banner.png",
 });
 
 // Parameters
 addParameters({
   options: {
-    theme
-  }
-})
+    theme,
+  },
+});
 
 // Decorators
 addDecorator(withA11y);
